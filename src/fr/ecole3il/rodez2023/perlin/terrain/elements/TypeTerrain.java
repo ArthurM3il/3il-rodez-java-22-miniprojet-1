109c48d93bb8c9fb4fd866aba8df623bf9770b4a
ArthurM3il <morenoa@3il.fr>
2024-02-19 18:33:32 +0100

    Adding to TypeTerrain class method getImage, toString override

---
+++ src/fr/ecole3il/rodez2023/perlin/terrain/elements/TypeTerrain.java
@@ -0,0 +1,32 @@
+package fr.ecole3il.rodez2023.perlin.terrain.elements;
+import fr.ecole3il.rodez2023.perlin.Utils;
+
+import java.awt.image.BufferedImage;
+
+public enum TypeTerrain{
+    COLLINES("hills.png"),
+    DESERT("desert.png"),
+    FORET_CONIFÈRES("coniferous_forest.png"),
+    FORET_FEUILLUS("deciduous_forest.png"),
+    MARAIS("marsh.png"),
+    MONTAGNE("mountain.png"),
+    OCEAN("ocean.png"),
+    PLAINE("plain.png"),
+    TOUNDRA("tundra.png");
+
+    private String nom;
+
+    TypeTerrain(String nom){
+        this.nom = nom;
+    }
+    public BufferedImage getImage(){
+        return Utils.chargerTuile(nom);
+    }
+
+
+
+    @Override
+    public String toString(){
+        return nom.substring(0, nom.length() - 4);
+    }
+}
