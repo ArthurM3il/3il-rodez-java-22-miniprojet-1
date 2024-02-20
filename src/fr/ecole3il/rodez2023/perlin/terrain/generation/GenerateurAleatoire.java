1c285f16caa4edc01460f7f5f51700e93b96613d
ArthurM3il <morenoa@3il.fr>
2024-02-20 22:00:33 +0100

    Fix genererTerrain

--- src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurAleatoire.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurAleatoire.java
@@ -1,5 +1,7 @@
 package fr.ecole3il.rodez2023.perlin.terrain.generation;
 
+import fr.ecole3il.rodez2023.perlin.math.BruitAleatoire;
+import fr.ecole3il.rodez2023.perlin.terrain.MauvaiseValeurException;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.Terrain;
 
 public class GenerateurAleatoire extends GenerateurCarte{
@@ -9,6 +11,15 @@ public class GenerateurAleatoire extends GenerateurCarte{
 
     @Override
     protected Terrain genererTerrain(int i, int j, int largeur, int hauteur) {
-        return null;
+        BruitAleatoire bruitAleatoire = new BruitAleatoire(this.getGraine(), 1.0);
+        double altitdude = bruitAleatoire.bruit2D(0.0,0.0);
+        double hydrometrie = bruitAleatoire.bruit2D(0.0,0.0);
+        double temperature = bruitAleatoire.bruit2D(0.0,0.0);
+
+        try {
+            return new Terrain(altitdude, hydrometrie, temperature);
+        } catch (MauvaiseValeurException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
