b2c62044a4d96d595ef916a8eb8d582af787de19
ArthurM3il <morenoa@3il.fr>
2024-02-22 15:39:45 +0100

    Fix genererTerrain

--- src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurPerlin.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/generation/GenerateurPerlin.java
@@ -4,19 +4,37 @@ import fr.ecole3il.rodez2023.perlin.math.BruitPerlin2D;
 import fr.ecole3il.rodez2023.perlin.terrain.MauvaiseValeurException;
 import fr.ecole3il.rodez2023.perlin.terrain.elements.Terrain;
 
+
 public class GenerateurPerlin extends GenerateurCarte{
+    /**
+     * Constructeur
+     * @param graine
+     */
     public GenerateurPerlin(long graine) {
         super(graine);
     }
 
+    /**
+     * Méthode générant le terrain
+     * @param i        La position verticale dans la carte.
+     * @param j        La position horizontale dans la carte.
+     * @param largeur  La largeur totale de la carte.
+     * @param hauteur  La hauteur totale de la carte.
+     * @return le terrain généré
+     * @throws MauvaiseValeurException
+     */
     @Override
     protected Terrain genererTerrain(int i, int j, int largeur, int hauteur) throws MauvaiseValeurException {
-        BruitPerlin2D bruitPerlin = new BruitPerlin2D(this.getGraine(),1);
-        double hydrometrie = bruitPerlin.bruit2D(0.0,0.0);
-        double temperature = 2 * bruitPerlin.bruit2D(0.0,0.0);
-        double altitude = 2 * temperature;
+        BruitPerlin2D bruitPerlinHydro = new BruitPerlin2D(getGraine(),1);
+        BruitPerlin2D bruitPerlinTemp = new BruitPerlin2D(2 * getGraine(),1);
+        BruitPerlin2D bruitPerlinAlt = new BruitPerlin2D(bruitPerlinTemp.getGraine() * 2,1);
+        double x = (double) i/largeur;
+        double y = (double) j/hauteur;
+        double hydrometrie = bruitPerlinHydro.bruit2D(x,y);
+        double temperature = bruitPerlinTemp.bruit2D(x,y);
+        double altitude = bruitPerlinAlt.bruit2D(x,y);
 
-        return new Terrain(altitude,hydrometrie,temperature);
+        return new Terrain(altitude,Math.abs(hydrometrie),Math.abs(temperature));
 
     }
 }
